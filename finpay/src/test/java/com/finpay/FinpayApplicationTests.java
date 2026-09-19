9d4adb563016ec881f1b229060292754552263a3
Omkar Ghodke <ghodkeomkar0012@gmail.com>
2026-09-19 11:59:26 +0530

    Initial FinPay project setup

---
+++ finpay/src/test/java/com/finpay/FinpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
